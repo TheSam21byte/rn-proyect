574ceda30b17f72e48799aba42b4aa044c8eded2
TheSam21byte <sanchezsamuel215@gmail.com>
2025-12-30 01:01:21 -0500

    cnn model

--- models/cnn/cnn_pure.py
+++ models/cnn/cnn_pure.py
@@ -1,16 +1,20 @@
 import torch.nn as nn
+from torch.nn.functional import dropout
+
 from .feature_extractor import CNNFeatureExtractor
 
 class CNNPure(nn.Module):
     """
-    CNN pura entrenada end-to-end.
+    CNN pura entrenada end-to-end, con regularización Dropout.
     """
-    def __init__(self, num_classes=2):
+    def __init__(self, num_classes=2, dropout_p=0.3):
         super().__init__()
         self.features = CNNFeatureExtractor()
+        self.dropout = nn.Dropout(p=dropout_p)
         self.classifier = nn.Linear(256, num_classes)
 
     def forward(self, x):
         x = self.features(x)
+        x = self.dropout(x)
         x = self.classifier(x)
         return x
